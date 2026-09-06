6aea186c0e71a375e06bc6d8d529849732bf4a5b
Tahsin0909 <tajisan789@gmail.com>
2026-09-06 15:02:50 +0600

    added main.java as a entry point

---
+++ src/main/java/com/library/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.library;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Library Management System");
+    }
+}
