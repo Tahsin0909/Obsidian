a1bbdb9a838fd8084f09a6003f0d32a185163085
Tahsin Zaman <134232536+Tahsin0909@users.noreply.github.com>
2026-09-21 20:13:55 +0600

    Update AuthService.java
    Co-Authored-By: Kamrul_Hasan_Rifat <97078040+Farabi9@users.noreply.github.com>

--- src/main/java/com/library/services/AuthService.java
+++ src/main/java/com/library/services/AuthService.java
@@ -29,6 +29,7 @@ public class AuthService {
         }
 
 System.out,println()
+System.out,println()
 
         // 1. Check Admin accounts (supports logging in via "admin" or "admin@mail.com")
         Admin admin = store.findAdminByUsernameOrEmail(trimmed);
