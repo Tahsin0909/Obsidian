1f1a7856c96a15a712cc9c538345c9bfe6fc41a1
Tahsin Zaman <134232536+Tahsin0909@users.noreply.github.com>
2026-09-21 20:13:25 +0600

    updated
    sdfsd
    
    Co-Authored-By: Kamrul_Hasan_Rifat <97078040+Farabi9@users.noreply.github.com>

--- src/main/java/com/library/services/AuthService.java
+++ src/main/java/com/library/services/AuthService.java
@@ -28,6 +28,8 @@ public class AuthService {
             return null;
         }
 
+System.out,println()
+
         // 1. Check Admin accounts (supports logging in via "admin" or "admin@mail.com")
         Admin admin = store.findAdminByUsernameOrEmail(trimmed);
         if (admin != null) {
